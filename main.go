6a198dc0490d741078fb35f5414307adc8f16e18
rchari66 <42802355+rchari66@users.noreply.github.com>
2018-12-04 10:32:13 +0530

    Mock Jira service

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(" Hello World!"))
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	query, ok := r.URL.Query()["q"]
+
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	if query[0] != "" {
+		w.Write([]byte("[{\"issueKey\":\"TEST-1\",\"fields\":{\"storyPoints\":10}},{\"issueKey\":\"TEST-2\",\"fields\":{\"storyPoints\":15}}]"))
+		// w.Write([]byte(""))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/rest/api/2/search", search)
+	if err := http.ListenAndServe(":8088", nil); err != nil {
+		panic(err)
+	}
+}
